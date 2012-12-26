775d6073be925bcd0eb68c10183a285e6ef37607
Guillaume Tucker <guillaume@mangoz.org>
2012-12-26 18:34:49 +0100

    add basic voice composition + start/stop button
    * parse sound resources names to sort them in categories
    * sound resources must be in res/raw/snd_CAT_ID where
      CAT is use as the caregory and ID is just to make the
      distinction between all the files
    * start/stop a thread when pressing the button which runs
      the sound sequence

--- src/mu/verdigris/hny/HNY.java
+++ src/mu/verdigris/hny/HNY.java
@@ -2,8 +2,12 @@ package mu.verdigris.hny;
 
 import java.lang.reflect.Field;
 import java.lang.IllegalAccessException;
-import java.util.List;
+import java.lang.InterruptedException;
+import java.lang.Thread;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import android.app.Activity;
@@ -21,7 +25,11 @@ public class HNY extends Activity {
     private Activity that;
     private Random rnd;
     private SoundPool sp;
-    private List<Integer> snd;
+    private Map<String, List<Integer>> snd;
+    private Thread seq;
+    private boolean runSeq;
+
+    /* ToDo: check return value of sp.play (stream id) and report error if 0 */
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,25 +39,33 @@ public class HNY extends Activity {
         this.that = this;
         this.rnd = new Random();
         this.sp = new SoundPool(16, AudioManager.STREAM_MUSIC, 0);
-        this.snd = new ArrayList<Integer>();
+        this.runSeq = false;
 
         try {
-            for (Field f: R.raw.class.getFields()) {
-                log(f.getName() + ": " + f.getInt(null));
-                this.snd.add(this.sp.load(this, f.getInt(null), 1));
-            }
+            this.buildSnd();
         } catch (IllegalAccessException e) {
             Log.e(HNY.TAG, "Oops: " + e.getMessage());
         }
 
         this.btnListener = new View.OnClickListener() {
                 public void onClick(View v) {
-                    final int sndIndex = rnd.nextInt(snd.size());
-                    final int streamId =
-                        sp.play(snd.get(sndIndex), 0.9f, 0.9f, 1, 0, 1.0f);
+                    HNY.this.runSeq = !HNY.this.runSeq;
+                    log("seq run: " + HNY.this.runSeq);
 
-                    if (streamId == 0)
-                        Log.e(HNY.TAG, "Failed to play sound: " + sndIndex);
+                    if (HNY.this.seq != null) {
+                        try {
+                            HNY.this.seq.join();
+                        } catch (InterruptedException e) {
+                            log("Oops: " + e.getMessage());
+                        }
+
+                        HNY.this.seq = null;
+                    }
+
+                    if (HNY.this.runSeq) {
+                        HNY.this.seq = new SequenceThread();
+                        HNY.this.seq.start();
+                    }
                 }
             };
 
@@ -59,8 +75,82 @@ public class HNY extends Activity {
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
         this.sp.release();
+        super.onDestroy();
+    }
+
+    private void buildSnd()
+        throws IllegalAccessException {
+
+        this.snd = new HashMap<String, List<Integer>>();
+
+        for (Field f: R.raw.class.getFields()) {
+            final String name = f.getName();
+            final int split1 = name.indexOf('_') + 1;
+            final int split2 = name.indexOf('_', split1);
+            final String sndPx = name.substring(split1, split2);
+            List<Integer> sndList;
+
+            if (!this.snd.containsKey(sndPx)) {
+                log("sound list: " + sndPx);
+                sndList = new ArrayList<Integer>();
+                this.snd.put(sndPx, sndList);
+            } else {
+                sndList = this.snd.get(sndPx);
+            }
+
+            sndList.add(this.sp.load(this, f.getInt(null), 1));
+        }
+    }
+
+    private class SequenceThread extends Thread {
+        private void playRandom(String sndName, int n) {
+            final List<Integer> sndList = HNY.this.snd.get(sndName);
+            final int id1 = sndList.get(HNY.this.rnd.nextInt(sndList.size()));
+            sp.play(id1, 0.9f, 0.9f, 1, 0, 1.0f);
+
+            if ((n == 1) || (sndList.size() == 1))
+                return;
+
+            int id2;
+
+            do {
+                id2 = sndList.get(HNY.this.rnd.nextInt(sndList.size()));
+            } while (id2 == id1);
+
+            sp.play(id2, 0.9f, 0.9f, 1, 0, 1.0f);
+        }
+
+        private void playAll(String sndName) {
+            for (int sndId: HNY.this.snd.get(sndName))
+                sp.play(sndId, 0.9f, 0.9f, 1, 0, 1.0f);
+        }
+
+        public void run() {
+            log("seq running");
+
+            while (HNY.this.runSeq) {
+                this.playRandom("voicehappy", 2);
+                this.doSleep(100);
+                sp.play(HNY.this.snd.get("guitar").get(0),
+                        0.9f, 0.9f, 1, 0, 1.0f);
+                this.doSleep(750);
+                this.playRandom("voicenew", 1);
+                this.doSleep(850);
+                this.playRandom("voiceyear", 2);
+                this.doSleep(1550);
+            }
+
+            log("seq done");
+        }
+
+        private void doSleep(int ms) {
+            try {
+                this.sleep(ms);
+            } catch (InterruptedException e) {
+                log("Oops: " + e.getMessage());
+            }
+        }
     }
 
     private static void log(String msg) {
